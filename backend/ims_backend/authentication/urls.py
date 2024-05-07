2452ac0a8ec41647067d3153c803bc2cf265cc74
rakeshAtWork <153895722+rakeshAtWork@users.noreply.github.com>
2024-05-07 16:57:46 +0530

    Third Commit

--- backend/ims_backend/authentication/urls.py
+++ backend/ims_backend/authentication/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('login/', HelloView.as_view(), name='login'),
     path('signup/', HelloView.as_view(), name='signup'),
+    path('dashboard/', HelloView.as_view(), name='dashboard')
 ]
